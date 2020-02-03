b25c6e765370bbc659d5dd624e26ab45f5a7330a
Prabhjot Singh Josan <prabhjosan16@gmail.com>
2020-02-03 09:23:52 -0500

    Addition du constructeur et des variables

--- src/Cv.java
+++ src/Cv.java
@@ -3,7 +3,23 @@ public class Cv {
 
 
 	
+	String nom ; 
+	String prenom ;
+	String formation;
+	int nbAnneesExp;
+	String[] competence = new String[0] ;
+	String attantes ;
+	
 
+	public Cv(String nom, String prenom, String formation, int nbAnneesExp, String[] competence, String attantes) {
+		this.nom = nom ; 
+		this.prenom = prenom;
+		this.formation = formation;
+		this.nbAnneesExp = nbAnneesExp;
+		this.competence = competence;
+		this.attantes = attantes;
+		
+	}
 	
 }
 
