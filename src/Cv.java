c28765ba78c03f18aa5b313c70e96e8d83d4b5e5
Prabhjot Singh Josan <prabhjosan16@gmail.com>
2020-02-03 09:27:35 -0500

    Addition de la méthode main()

--- src/Cv.java
+++ src/Cv.java
@@ -1,50 +1,53 @@
 
 public class Cv {
 
+	public static void main(String[] args) {
 
-	
-	String nom ; 
-	String prenom ;
+		String[] competence = { "Gentil", "Has friends", "Better than Daniel" };
+
+		Cv cvPRABH = new Cv("Josan", "Prabhjot Singh", "Informatique", 0, competence, "Bien coder et apprendre GitHub");
+
+		cvPRABH.affiche();
+	}
+
+	String nom;
+	String prenom;
 	String formation;
 	int nbAnneesExp;
-	String[] competence = new String[0] ;
-	String attantes ;
-	
+	String[] competence = new String[0];
+	String attantes;
 
 	public Cv(String nom, String prenom, String formation, int nbAnneesExp, String[] competence, String attantes) {
-		this.nom = nom ; 
+		this.nom = nom;
 		this.prenom = prenom;
 		this.formation = formation;
 		this.nbAnneesExp = nbAnneesExp;
 		this.competence = competence;
 		this.attantes = attantes;
-		
+
 	}
-	
+
 	public void affiche() {
-		
-		System.out.println("*************************") ;
+
+		System.out.println("*************************");
 
 		System.out.println("Nom : " + nom);
-		
+
 		System.out.println("Prénom : " + prenom);
-		
+
 		System.out.println("Formation : " + formation);
-		
+
 		System.out.println("nombre d'années d'expériences : " + nbAnneesExp);
-		
+
 		System.out.println("Competence : ");
-		
+
 		for (int i = 0; i < competence.length; i++) {
-			
+
 			System.out.println("- " + competence[i]);
-		
+
 		}
-		
-		
+
 		System.out.println("Attantes vis à vis le cours 4B4: " + attantes);
 	}
-	
+
 }
-
-
