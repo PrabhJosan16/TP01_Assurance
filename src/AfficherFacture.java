1fc81f312699213176e47393a152227b6dfe339a
Prabhjot Singh Josan <prabhjosan16@gmail.com>
2020-04-21 11:23:57 -0400

    petite modification de AfficherFacture

--- src/AfficherFacture.java
+++ src/AfficherFacture.java
@@ -14,26 +14,23 @@ public class AfficherFacture {
 	 * @param fileNameAndExtension
 	 */
 
-	public double pourcentageTaxesTPS = 0.09975;
-	public double pourcentageTaxesTVQ = 0.05;
+	private double pourcentageTaxesTPS = 0.09975, pourcentageTaxesTVQ = 0.05;
+	private String strJour, tempsHeures, listeClients = "", listePlats = "", listeCommandes = "";
+	private String[] arrClients, arrPlats, arrCommandes;
 
 	public AfficherFacture(String fileNameAndExtension) {
-		String listeClients = "";
-		String listePlats = "";
-		String listeCommandes = "";
-
 		try {
 			// Ceci crée un nouveau fichier avec le nom complet du fichier
 			File file = new File(fileNameAndExtension);
 			// créer arrClients, arrPlats et arrCommandes
-			Scanner inputStream = new Scanner(file); 
+			Scanner inputStream = new Scanner(file);
 			int typeData = 0;
 			int nbLigneCourante = 0;
 			while (inputStream.hasNext()) {
 				String lineRead = inputStream.nextLine();
-				lineRead = lineRead.trim().replaceAll("\\s+", " "); // enleve les spaces de trop dans la ligne
+				// enleve les spaces de trop dans la ligne
+				lineRead = lineRead.trim().replaceAll("\\s+", " ");
 				nbLigneCourante += 1;
-
 				if (lineRead.contains("Clients :")) {
 					typeData = 1;
 				} else if (lineRead.contains("Plats :")) {
@@ -44,34 +41,24 @@ public class AfficherFacture {
 					if (verifierNomClient(lineRead, 1)) {
 						listeClients += lineRead + ";";
 					} else {
-						System.out.println("**********************************************************");
-						System.out.println("Le nom du client est incorrect dans la ligne " + nbLigneCourante
-								+ " pour le client : " + lineRead);
-						System.out.println("**********************************************************");
+						afficherMessageErrPourSection(nbLigneCourante, lineRead, "client");
 					}
 				} else if (typeData == 2 && !(lineRead.contains("Plats :"))) {
 					if (verifierLignePlatEtComm(lineRead, 2)) {
 						listePlats += lineRead + ";";
 					} else {
-						System.out.println("**********************************************************");
-						System.out.println("Il y a plus ou moins que deux elements dans la ligne " + nbLigneCourante
-								+ " pour le plat : " + lineRead);
-						System.out.println("**********************************************************");
+						afficherMessageErrPourSection(nbLigneCourante, lineRead, "plat");
 					}
 				} else if (typeData == 3 && !(lineRead.contains("Commandes :")) && !(lineRead.contains("Fin"))) {
 					if (verifierLignePlatEtComm(lineRead, 3)) {
 						listeCommandes += lineRead + ";";
 					} else {
-						System.out.println("**********************************************************");
-						System.out.println("Il y a plus ou moins que trois elements dans la ligne " + nbLigneCourante
-								+ " pour la commande : " + lineRead);
-						System.out.println("**********************************************************");
+						afficherMessageErrPourSection(nbLigneCourante, lineRead, "commande");
 					}
 				}
 			}
-
-			if (listeClients != "" || listePlats != "" || listeCommandes != "") {
-				creerUnFichierChqClinet(listeClients, listePlats, listeCommandes);
+			if (listeClients != "" && listePlats != "" && listeCommandes != "") {
+				creerUnFichierChqClinet();
 			} else {
 				System.out.println("Le fichier est vide dans une ou plusieurs sections (plat, client, Commande).");
 			}
@@ -80,6 +67,12 @@ public class AfficherFacture {
 		}
 	}
 
+	private void afficherMessageErrPourSection(int nbLigneCourante , String lineRead, String typeDonnee ) {
+		System.out.println("**********************************************************");
+		System.out.println("Il y a plus ou moins que trois elements dans la ligne " + nbLigneCourante
+				+ " pour la " +typeDonnee  + " : " + lineRead);
+		System.out.println("**********************************************************");
+	}
 	/**
 	 * Cette méthode crée un fichier pour chaque client
 	 * 
@@ -87,50 +80,48 @@ public class AfficherFacture {
 	 * @param listePlats
 	 * @param listeCommandes
 	 */
-	public void creerUnFichierChqClinet(String listeClients, String listePlats, String listeCommandes) {
-		String[] arrClients = listeClients.split(";");
-		String[] arrPlats = listePlats.split(";");
-		String[] arrCommandes = listeCommandes.split(";");
-		int nbClients = arrClients.length;
-		Client[] listeClient = creerChqClient(arrClients, arrPlats, arrCommandes, nbClients);
+	private void creerUnFichierChqClinet() {
+		arrClients = listeClients.split(";");
+		arrPlats = listePlats.split(";");
+		arrCommandes = listeCommandes.split(";");
+		Client[] listeClient = creerChqClient(arrClients.length);
+
+		SimpleDateFormat formatterDate = new SimpleDateFormat("dd-M-yyyy");
+		strJour = formatterDate.format(new Date());
+		SimpleDateFormat formatHeure = new SimpleDateFormat("HH");
+		tempsHeures = formatHeure.format(new Date());
 		
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
-		String jourAchatStr = formatter.format(new Date());
-		System.out.println( jourAchatStr );
-		String recuClientFinal = jourAchatStr + "\n";
-		
+		String recuClientFinal = (strJour + " " + tempsHeures + "h");
 		for (int i = 0; i < listeClient.length; i++) {
 			Commande[] commandeArray = listeClient[i].getCommande();
-			
 			if (commandeArray.length != 0) {
 				double prixTotalPartiel = calculerPrixTotalPartiel(commandeArray);
 				if (prixTotalPartiel <= 0) {
-					System.out.println("Le prix total pour le client " + listeClient[i].getNomClient()
-							+ "  ne peut pas être 0 ou un chiffre négative");
 				} else {
 					recuClientFinal += afficherUnClient(listeClient[i], prixTotalPartiel);
-					System.out.println(afficherUnClient(listeClient[i], prixTotalPartiel));
 				}
 			} else {
 				System.out.println("Le client " + listeClient[i].getNomClient() + " n'a rien acheté.");
 			}
-
 		}
+		System.out.println(recuClientFinal);
+		creerFichierFacture(recuClientFinal);
+	}
 
-		SimpleDateFormat formatter2 = new SimpleDateFormat("dd-M-yyyy");
-		String strJour = formatter2.format(new Date());
-		SimpleDateFormat formatHours = new SimpleDateFormat("HH");
-		String tempsHeures = formatHours.format(new Date());
-
+	/**
+	 * Cette methode crée un facture en format txt
+	 * 
+	 * @param recuClientFinal
+	 */
+	private void creerFichierFacture(String recuClientFinal) {
 		try {
-			FileWriter myWriter = new FileWriter("Facture_du_" + strJour + "_" + tempsHeures + "h_" + ".txt");
+			FileWriter myWriter = new FileWriter("Facture_du_" + strJour + "_" + tempsHeures + "h" + ".txt");
 			myWriter.write(recuClientFinal);
 			myWriter.close();
 		} catch (IOException e) {
 			System.out.println("Le fichier de sortie n'a pas peut etre crée ");
 			e.printStackTrace();
 		}
-
 	}
 
 	/**
@@ -166,19 +157,15 @@ public class AfficherFacture {
 	/**
 	 * Crée la facture pour chaque client
 	 * 
-	 * @param arrClients
-	 * @param arrPlats
-	 * @param arrCommandes
 	 * @param nbClients
-	 * @return Facture[] arrayFacture
+	 * @return
 	 */
 
-	private Client[] creerChqClient(String[] arrClients, String[] arrPlats, String[] arrCommandes, int nbClients) {
+	private Client[] creerChqClient(int nbClients) {
 		Client[] arrayClient = null;
 		ArrayList<Client> clientArrayList = new ArrayList<Client>();
 		for (int i = 0; i < arrClients.length; i++) {
 			String nomClient = arrClients[i];
-			Plats[] tousLesPlatsMenu = tousPlats(arrPlats);
 			// Ce bloque crée tous les nomPlat , nbPlat et nomClient
 			ArrayList<String> nomPlatArryList = new ArrayList<String>();
 			ArrayList<Integer> nbPlatArryList = new ArrayList<Integer>();
@@ -192,31 +179,7 @@ public class AfficherFacture {
 					nomClientArryList.add(arrCommandeSplited[0]);
 				}
 			}
-			String[] nomPlatCommandeClient = nomPlatArryList.toArray(new String[0]);
-			int[] quantiteCommandeClient = nbPlatArryList.stream().mapToInt(Integer::intValue).toArray();
-			String[] nomClientCommandeClient = nomClientArryList.toArray(new String[0]);
-			
-			// Ce bloque crée toutes les commandes avec
-			ArrayList<Commande> commandeArrayList = new ArrayList<Commande>();
-			for (int j = 0; j < nomPlatCommandeClient.length; j++) {
-				for (int x = 0; x < tousLesPlatsMenu.length; x++) {
-					if (tousLesPlatsMenu[x].getNomPlats().equals(nomPlatCommandeClient[j])) {
-						if (quantiteCommandeClient[j] <= 0) {
-							
-							System.out.print("La commande : " + nomClientCommandeClient[j] + " "
-									+ tousLesPlatsMenu[x].getNomPlats() + " " + quantiteCommandeClient[j]);
-							
-							System.out.println(" ne peut pas avoir une quanitité inferieur ou égale à 0");
-							
-						} else {
-
-							commandeArrayList.add(new Commande(tousLesPlatsMenu[x], quantiteCommandeClient[j],
-									nomClientCommandeClient[j]));
-						}
-					}
-				}
-			}
-			Commande[] CommandeDeChqClient = commandeArrayList.toArray(new Commande[0]);
+			Commande[] CommandeDeChqClient = avoirToutesCommandes(nomPlatArryList, nbPlatArryList, nomClientArryList);
 			// Cette ligne crée le Client current
 			clientArrayList.add(new Client(nomClient, CommandeDeChqClient));
 		}
@@ -224,11 +187,35 @@ public class AfficherFacture {
 		return arrayClient;
 	}
 
-	public Plats[] tousPlats(String[] arrPlats) {
-		
+	private Commande[] avoirToutesCommandes(ArrayList<String> nomPlatArryList, ArrayList<Integer> nbPlatArryList,
+			ArrayList<String> nomClientArryList) {
+		Plats[] tousLesPlatsMenu = tousPlats(arrPlats);
+		String[] nomPlatCommandeClient = nomPlatArryList.toArray(new String[0]);
+		int[] quantiteCommandeClient = nbPlatArryList.stream().mapToInt(Integer::intValue).toArray();
+		String[] nomClientCommandeClient = nomClientArryList.toArray(new String[0]);
+		// Ce bloque crée toutes les commandes avec
+		ArrayList<Commande> commandeArrayList = new ArrayList<Commande>();
+		for (int j = 0; j < nomPlatCommandeClient.length; j++) {
+			for (int x = 0; x < tousLesPlatsMenu.length; x++) {
+				if (tousLesPlatsMenu[x].getNomPlats().equals(nomPlatCommandeClient[j])) {
+					if (quantiteCommandeClient[j] <= 0) {
+						System.out.print("La commande : " + nomClientCommandeClient[j] + " "
+								+ tousLesPlatsMenu[x].getNomPlats() + " " + quantiteCommandeClient[j]);
+						System.out.println(" ne peut pas avoir une quanitité inferieur ou égale à 0");
+					} else {
+
+						commandeArrayList.add(new Commande(tousLesPlatsMenu[x], quantiteCommandeClient[j],
+								nomClientCommandeClient[j]));
+					}
+				}
+			}
+		}
+		return commandeArrayList.toArray(new Commande[0]);
+	}
+
+	private Plats[] tousPlats(String[] arrPlats) {
 		// Ce bloque crée chaque plat existant
 		ArrayList<Plats> platsArrayList = new ArrayList<Plats>();
-		// int nbPlats = arrPlats.length ;
 		for (int j = 0; j < arrPlats.length; j++) {
 			String arrPlatCurrent = arrPlats[j];
 			String[] arrPlatSplited = arrPlatCurrent.split(" ");
@@ -236,29 +223,15 @@ public class AfficherFacture {
 			double prixPlat = Double.parseDouble(arrPlatSplited[1]);
 			platsArrayList.add(new Plats(nomPlat, prixPlat));
 		}
-		// The next tranforms array list Plat into an array of Plat
+		// La prochaines ligne tranforme Arraylist Plat en array de Plat
 		Plats[] platchqCommandeDuClient = platsArrayList.toArray(new Plats[0]);
 		return platchqCommandeDuClient;
 	}
 
-	public double calculerPrixTotalPartiel(Commande[] commandeArray) {
-
-		double prixTotalPartiel = 0;
-		for (int j = 0; j < commandeArray.length; j++) {
-			prixTotalPartiel += commandeArray[j].getPlats().getPrixPlats() * commandeArray[j].getNbPlats();
-		}
-		return prixTotalPartiel;
-	}
-
-	public String afficherUnClient(Client client, double prixTotalPartiel) {
+	private String afficherUnClient(Client client, double prixTotalPartiel) {
 		String commandePourUnClient = "";
 		Commande[] commandeArray = client.getCommande();
-
-	
-		
-		
-		commandePourUnClient = "____________________________________\n";
-		
+		commandePourUnClient = "\n____________________________________\n";
 		commandePourUnClient += "*********************\n";
 		commandePourUnClient += "Nom du Client :  " + client.getNomClient() + "\n";
 		for (int i = 0; i < commandeArray.length; i++) {
@@ -267,15 +240,15 @@ public class AfficherFacture {
 			commandePourUnClient += "Prix par plat : " + commandeArray[i].getPlats().getPrixPlats() + "\n";
 			commandePourUnClient += "*********************" + "\n";
 		}
-		commandePourUnClient += "Total Partiel : " + prixTotalPartiel + "\n";
+		commandePourUnClient += "Total Partiel : " + prixTotalPartiel + "$\n";
 		commandePourUnClient += "Total "
 				+ arrondir(caluclerTotal(prixTotalPartiel, calucleraxes(prixTotalPartiel, pourcentageTaxesTPS),
 						calucleraxes(prixTotalPartiel, pourcentageTaxesTVQ)), 2)
-				+ "\n";
+				+ "$\n";
 		return commandePourUnClient;
 	}
 
-	// Cette section est pour calculer les taxes et bieb plus
+	// Cette section est pour calculer les taxes et bien plus
 
 	private double calucleraxes(double totalPartiel, double pourcentageTax) {
 		return totalPartiel * pourcentageTax;
@@ -285,7 +258,15 @@ public class AfficherFacture {
 		return totalPartiel + tps + tvq;
 	}
 
-	public static double arrondir(double valeurPorArrondir, int chiffresApresVirgule) {
+	private double calculerPrixTotalPartiel(Commande[] commandeArray) {
+		double prixTotalPartiel = 0;
+		for (int j = 0; j < commandeArray.length; j++) {
+			prixTotalPartiel += commandeArray[j].getPlats().getPrixPlats() * commandeArray[j].getNbPlats();
+		}
+		return prixTotalPartiel;
+	}
+
+	private static double arrondir(double valeurPorArrondir, int chiffresApresVirgule) {
 		if (chiffresApresVirgule < 0) {
 			throw new IllegalArgumentException();
 		}
